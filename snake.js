307fa30cb3d1abc5fdaeaab1ec5cb06970c6f877
Anny <annytakumi@gmail.com>
2026-08-11 18:38:32 +0200

    añadiendo la serpiente

--- snake.js
+++ snake.js
@@ -1,9 +1,23 @@
+const playboard = document.querySelector(".play-board");
+
+let foodX, foodY;
+let snakeX = 5, snakeY = 20;
+
+const changeFoodPosition = () =>
+{
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
+}
+
 const initGame = () => 
 {
-    let htmlMarkup = '<div class = "food" style"grid-area"></div>'
+    let htmlMarkup = '<div class = "food" style"grid-area: ${foodY} / ${foodX}"></div>'
+    htmlMarkup += '<div class = "head" style"grid-area: ${snakeY} / ${snakeX}"></div>'
+    playboard.innerHTML = htmlMarkup;
 
 
 
 }
+changeFoodPosition();
  
 initGame();
