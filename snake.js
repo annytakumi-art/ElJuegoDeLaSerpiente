aad9fa986c25be567d47627bcce1a4ba472beede
Anny <annytakumi@gmail.com>
2026-08-12 18:34:12 +0200

    añadimos movimiento en ejes Y y X, añadimos velocidad e inicializamos cambio de posición de la comida

--- snake.js
+++ snake.js
@@ -1,7 +1,9 @@
-const playboard = document.querySelector(".play-board");
+const playBoard = document.querySelector(".play-board");
 
 let foodX, foodY;
-let snakeX = 5, snakeY = 20;
+let snakeX = 5;
+let snakeY = 20;
+let velocityX = 0, velocityY = 0;
 
 const changeFoodPosition = () =>
 {
@@ -9,15 +11,47 @@ const changeFoodPosition = () =>
     foodY = Math.floor(Math.random() * 30) + 1;
 }
 
+const changeDirection = (e) =>
+    {
+        if(e.key === "ArrowUp")
+        {
+            velocityX = 0;
+            velocityY = -1;
+        }else if(e.key === "ArrowDown")
+        {
+            velocityX = 0;
+            velocityY = +1;
+        }else if(e.key === "ArrowLeft")
+        {
+            velocityX = -1;
+            velocityY = 0;
+        } else if(e.key === "ArrowRight")
+        {
+            velocityX = 1;
+            velocityY = 0;
+        }
+        //initGame();
+    } 
+    
 const initGame = () => 
 {
-    let htmlMarkup = '<div class = "food" style"grid-area: ${foodY} / ${foodX}"></div>'
-    htmlMarkup += '<div class = "head" style"grid-area: ${snakeY} / ${snakeX}"></div>'
-    playboard.innerHTML = htmlMarkup;
+    let htmlMarkup = `<div class = "food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+
+    if(snakeX === foodX && snakeY === foodY)
+    {
+        changeFoodPosition();
+    }
+
+    snakeX += velocityX;
+    snakeY += velocityY;
+
+    htmlMarkup += `<div class = "head" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
+    playBoard.innerHTML = htmlMarkup;
 
 
 
 }
 changeFoodPosition();
- 
-initGame();
+ setInterval(initGame, 125)
+
+document.addEventListener("keydown", changeDirection);
