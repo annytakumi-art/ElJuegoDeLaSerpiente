777e5cb08d156626db2d00b231e1ed7714a02d00
Anny <annytakumi@gmail.com>
2026-08-13 16:33:01 +0200

    añado array a la serpiente para que empiece a alargarse

--- snake.js
+++ snake.js
@@ -5,6 +5,8 @@ let snakeX = 5;
 let snakeY = 20;
 let velocityX = 0, velocityY = 0;
 
+let snakeBody = [];
+
 const changeFoodPosition = () =>
 {
     foodX = Math.floor(Math.random() * 30) + 1;
@@ -40,12 +42,24 @@ const initGame = () =>
     if(snakeX === foodX && snakeY === foodY)
     {
         changeFoodPosition();
+        snakeBody.push([foodX, foodY]);
     }
+    for(let i= snakeBody.length -1; i > 0; i-- )
+    {
+        snakeBody[i] = snakeBody[i - 1];
+    }
+
+    snakeBody[0] = [snakeX, snakeY];
 
     snakeX += velocityX;
     snakeY += velocityY;
 
-    htmlMarkup += `<div class = "head" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
+    for(let i = 0; i < snakeBody.length; i++)
+    {
+    htmlMarkup += `<div class = "head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+    }
+
+    
     playBoard.innerHTML = htmlMarkup;
 
 
